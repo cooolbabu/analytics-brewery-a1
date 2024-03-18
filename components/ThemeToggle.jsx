6878f6c2ac8a56c1a688813b743ed7c98ba40b1c
Sreenivas Angara <cooolbabu@yahoo.com>
2024-03-18 09:15:05 -0500

    Boilerplate completed

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -4,22 +4,22 @@ import { BsMoonFill, BsSunFill } from "react-icons/bs";
 import { useState } from "react";
 
 const themes = {
-  dark: "dim",
-  light: "nord",
+  dim: "dim",
+  nord: "nord",
 };
 
 function ThemeToggle() {
-  const [theme, setTheme] = useState(themes.light);
+  const [theme, setTheme] = useState(themes.nord);
 
   const toggleTheme = () => {
-    const newTheme = theme === themes.dark ? themes.light : themes.dark;
+    const newTheme = theme === themes.dim ? themes.nord : themes.dim;
     document.documentElement.setAttribute("data-theme", newTheme);
     setTheme(newTheme);
   };
 
   return (
     <button onClick={toggleTheme} className="btn btn-sm btn-outline">
-      {theme === "dark" ? (
+      {theme === "dim" ? (
         <BsMoonFill className="h-4 w-4 " />
       ) : (
         <BsSunFill className="h-4 w-4" />
